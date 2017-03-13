3e628a180e42248d2238fb43ace9c498adc5c17d
邓启宋 <dengqs@physicalpoint.com>
2017-03-13 10:28:12 +0800

    webviewH5加载优化

---
+++ app/src/main/java/com/hyh/www/common/module/Html5Activity.java
@@ -0,0 +1,159 @@
+package com.hyh.www.common.module;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.Message;
+import android.support.annotation.Nullable;
+import android.view.ViewGroup;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
+
+import com.hyh.www.common.R;
+import com.hyh.www.common.app.BaseActivity;
+
+/**
+ * 作者：Denqs on 2017/3/13.
+ */
+
+public class Html5Activity extends BaseActivity{
+    private String mUrl = "https://www.amazon.cn/?tag=sosohydracn-23&ref=pz_ic_M_Gateway_N_170105_7480";
+    private LinearLayout mLayout;
+    private WebView mWebView;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_h5);
+        initView();
+    }
+
+    public void initView() {
+        mLayout = (LinearLayout) findViewById(R.id.web_layout);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        mWebView = new WebView(getApplicationContext());
+        mWebView.setLayoutParams(params);
+        mLayout.addView(mWebView);
+        WebSettings mWebSettings = mWebView.getSettings();
+        mWebSettings.setSupportZoom(true);
+        mWebSettings.setLoadWithOverviewMode(true);
+        mWebSettings.setUseWideViewPort(true);
+        mWebSettings.setDefaultTextEncodingName("utf-8");
+        mWebSettings.setLoadsImagesAutomatically(true);
+        //调用JS方法.安卓版本大于17,加上注解 @JavascriptInterface
+        mWebSettings.setJavaScriptEnabled(true);
+        saveData(mWebSettings);
+        newWin(mWebSettings);
+        mWebView.setWebChromeClient(webChromeClient);
+        mWebView.setWebViewClient(webViewClient);
+        mWebView.loadUrl(mUrl);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mWebView.onPause();
+        mWebView.pauseTimers(); //小心这个！！！暂停整个 WebView 所有布局、解析、JS。
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mWebView.onResume();
+        mWebView.resumeTimers();
+    }
+    /**
+     * 多窗口的问题
+     */
+    private void newWin(WebSettings mWebSettings) {
+        //html中的_bank标签就是新建窗口打开，有时会打不开，需要加以下
+        //然后 复写 WebChromeClient的onCreateWindow方法
+        mWebSettings.setSupportMultipleWindows(false);
+        mWebSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()){
+            mWebView.goBack();
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+    /**
+     * HTML5数据存储
+     */
+    private void saveData(WebSettings mWebSettings) {
+        //有时候网页需要自己保存一些关键数据,Android WebView 需要自己设置
+        mWebSettings.setDomStorageEnabled(true);
+        mWebSettings.setDatabaseEnabled(true);
+        mWebSettings.setAppCacheEnabled(true);
+        String appCachePath = getApplicationContext().getCacheDir().getAbsolutePath();
+        mWebSettings.setAppCachePath(appCachePath);
+    }
+    WebViewClient webViewClient = new WebViewClient() {
+
+        /**
+         * 多页面在同一个WebView中打开，就是不新建activity或者调用系统浏览器打开
+         */
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+    };
+
+    WebChromeClient webChromeClient = new WebChromeClient() {
+
+        //=========HTML5定位==========================================================
+        //需要先加入权限
+        //<uses-permission android:name="android.permission.INTERNET"/>
+        //<uses-permission android:name="android.permission.ACCESS_FINE_LOCATION"/>
+        //<uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION"/>
+        @Override
+        public void onReceivedIcon(WebView view, Bitmap icon) {
+            super.onReceivedIcon(view, icon);
+        }
+
+        @Override
+        public void onGeolocationPermissionsHidePrompt() {
+            super.onGeolocationPermissionsHidePrompt();
+        }
+
+        @Override
+        public void onGeolocationPermissionsShowPrompt(final String origin, final GeolocationPermissions.Callback callback) {
+            callback.invoke(origin, true, false);//注意个函数，第二个参数就是是否同意定位权限，第三个是是否希望内核记住
+            super.onGeolocationPermissionsShowPrompt(origin, callback);
+        }
+        //=========HTML5定位==========================================================
+
+        //=========多窗口的问题==========================================================
+        @Override
+        public boolean onCreateWindow(WebView view, boolean isDialog, boolean isUserGesture, Message resultMsg) {
+            WebView.WebViewTransport transport = (WebView.WebViewTransport) resultMsg.obj;
+            transport.setWebView(view);
+            resultMsg.sendToTarget();
+            return true;
+        }
+        //=========多窗口的问题==========================================================
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mWebView != null) {
+            mWebView.clearHistory();
+            ((ViewGroup) mWebView.getParent()).removeView(mWebView);
+            mWebView.loadUrl("about:blank");
+            mWebView.stopLoading();
+            mWebView.setWebChromeClient(null);
+            mWebView.setWebViewClient(null);
+            mWebView.destroy();
+            mWebView = null;
+        }
+    }
+}
--- app/src/main/java/com/hyh/www/common/module/fragment/WebFragment.java
+++ app/src/main/java/com/hyh/www/common/module/fragment/WebFragment.java
@@ -1,5 +1,6 @@
 package com.hyh.www.common.module.fragment;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Message;
@@ -17,15 +18,13 @@ import android.widget.LinearLayout;
 import com.hyh.www.common.MainActivity;
 import com.hyh.www.common.R;
 import com.hyh.www.common.app.BaseFragment;
+import com.hyh.www.common.module.Html5Activity;
 
 /**
  * 作者：Denqs on 2017/3/7.
  */
 
 public class WebFragment extends BaseFragment {
-    private String mUrl = "https://jx.tmall.com/?ali_trackid=2:mm_26632357_8426500_28318607:1488879764_3k2_1082375922&e=oRnUU9Gq9rxw4vFB6t2Z2iperVdZeJviK7Vc7tFgwiFRAdhuF14FMdG1qk4zRQOZt4hWD5k2kjPy80H7gHL6a4ddreqVkCMUpBN_LcdyCX4-z0jsbgbe5aUuZxIcp9pfcV_yRUpq-QyOTe7uzUz56eup4Whmf9f1uFwU83aX2tMtfHsVWlcmHwF-oVVa2fkXVZirzcuwtvwGypc3vHuAewra5RIrlmtbMZwYqAxX-uUQGBTbgn0tmg&type=2&tkFlag=1";
-    private LinearLayout mLayout;
-    private WebView mWebView;
 
     public static WebFragment newInstance(String fragConent) {
         Bundle args = new Bundle();
@@ -45,123 +44,14 @@ public class WebFragment extends BaseFragment {
     }
 
     public void initView(View view) {
-        mLayout = (LinearLayout) view.findViewById(R.id.web_layout);
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        mWebView = new WebView(getContext().getApplicationContext());
-        mWebView.setLayoutParams(params);
-        mLayout.addView(mWebView);
-        WebSettings mWebSettings = mWebView.getSettings();
-        mWebSettings.setSupportZoom(true);
-        mWebSettings.setLoadWithOverviewMode(true);
-        mWebSettings.setUseWideViewPort(true);
-        mWebSettings.setDefaultTextEncodingName("utf-8");
-        mWebSettings.setLoadsImagesAutomatically(true);
-        //调用JS方法.安卓版本大于17,加上注解 @JavascriptInterface
-        mWebSettings.setJavaScriptEnabled(true);
-        saveData(mWebSettings);
-        newWin(mWebSettings);
-        mWebView.setWebChromeClient(webChromeClient);
-        mWebView.setWebViewClient(webViewClient);
-        mWebView.loadUrl(mUrl);
+         view.findViewById(R.id.yam).setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 startActivity(new Intent(getActivity(), Html5Activity.class));
+             }
+         });
     }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        mWebView.onPause();
-        mWebView.pauseTimers(); //小心这个！！！暂停整个 WebView 所有布局、解析、JS。
-    }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        mWebView.onResume();
-        mWebView.resumeTimers();
-    }
-
-    /**
-     * 多窗口的问题
-     */
-    private void newWin(WebSettings mWebSettings) {
-        //html中的_bank标签就是新建窗口打开，有时会打不开，需要加以下
-        //然后 复写 WebChromeClient的onCreateWindow方法
-        mWebSettings.setSupportMultipleWindows(false);
-        mWebSettings.setJavaScriptCanOpenWindowsAutomatically(true);
-    }
-
-    /**
-     * HTML5数据存储
-     */
-    private void saveData(WebSettings mWebSettings) {
-        //有时候网页需要自己保存一些关键数据,Android WebView 需要自己设置
-        mWebSettings.setDomStorageEnabled(true);
-        mWebSettings.setDatabaseEnabled(true);
-        mWebSettings.setAppCacheEnabled(true);
-        String appCachePath = getContext().getApplicationContext().getCacheDir().getAbsolutePath();
-        mWebSettings.setAppCachePath(appCachePath);
-    }
-
-    WebViewClient webViewClient = new WebViewClient() {
-
-        /**
-         * 多页面在同一个WebView中打开，就是不新建activity或者调用系统浏览器打开
-         */
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
-        }
-
-    };
-
-    WebChromeClient webChromeClient = new WebChromeClient() {
-
-        //=========HTML5定位==========================================================
-        //需要先加入权限
-        //<uses-permission android:name="android.permission.INTERNET"/>
-        //<uses-permission android:name="android.permission.ACCESS_FINE_LOCATION"/>
-        //<uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION"/>
-        @Override
-        public void onReceivedIcon(WebView view, Bitmap icon) {
-            super.onReceivedIcon(view, icon);
-        }
-
-        @Override
-        public void onGeolocationPermissionsHidePrompt() {
-            super.onGeolocationPermissionsHidePrompt();
-        }
-
-        @Override
-        public void onGeolocationPermissionsShowPrompt(final String origin, final GeolocationPermissions.Callback callback) {
-            callback.invoke(origin, true, false);//注意个函数，第二个参数就是是否同意定位权限，第三个是是否希望内核记住
-            super.onGeolocationPermissionsShowPrompt(origin, callback);
-        }
-        //=========HTML5定位==========================================================
-
-        //=========多窗口的问题==========================================================
-        @Override
-        public boolean onCreateWindow(WebView view, boolean isDialog, boolean isUserGesture, Message resultMsg) {
-            WebView.WebViewTransport transport = (WebView.WebViewTransport) resultMsg.obj;
-            transport.setWebView(view);
-            resultMsg.sendToTarget();
-            return true;
-        }
-        //=========多窗口的问题==========================================================
-    };
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (mWebView != null) {
-            mWebView.clearHistory();
-            ((ViewGroup) mWebView.getParent()).removeView(mWebView);
-            mWebView.loadUrl("about:blank");
-            mWebView.stopLoading();
-            mWebView.setWebChromeClient(null);
-            mWebView.setWebViewClient(null);
-            mWebView.destroy();
-            mWebView = null;
-        }
-    }
 
 }
