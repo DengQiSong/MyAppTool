20d22ba560ec82ecae844d6687a763b4f213b2f4
DengQiSong <244198779@qq.com>
2017-07-07 14:07:27 +0800

    add bug

--- app/src/main/java/com/hyh/www/common/module/fragment/Test/TestPresenter.java
+++ app/src/main/java/com/hyh/www/common/module/fragment/Test/TestPresenter.java
@@ -7,7 +7,10 @@ import com.dqs.http_library.callback.HttpBaseObserver;
 import com.hyh.www.common.MainActivity;
 import com.hyh.www.common.config.Url;
 import com.hyh.www.common.model.test.TestModel;
+import com.hyh.www.common.module.vo.Subject;
 import com.hyh.www.common.utils.DownloadUtils;
+
+import java.util.List;
 
 /**
  * 作者：Denqs on 2017/3/14.
@@ -23,11 +26,11 @@ public class TestPresenter implements TestContract.Presenter {
 
     @Override
     public void doGet(Context context) {
-        TestModel.getInstance().execute(0, 100, new HttpBaseObserver((MainActivity)context) {
+        TestModel.getInstance().execute(0, 100, new HttpBaseObserver<List<Subject>>((MainActivity)context) {
             @Override
-            protected void _onSuccess(Object o) {
-                Log.e("TAG",o.toString());
-        }
+            protected void _onSuccess(List<Subject> subjects) {
+                Log.e("TAG","数据总数"+subjects .size()+"，第一条数据title "+subjects.get(0).getTitle());
+            }
         });
     }
 
